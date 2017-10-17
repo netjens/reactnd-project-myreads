11ab73927d62c651d7a43bd49c8244328936352a
Jens Gebhardt <dt0a35@canda.com>
2017-10-17 17:49:45 +0200

    feat: add new components for book-search and book-list

---
+++ src/ListBooks.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+class ListBooks extends Component {
+    render(){
+        return (
+            <p>List Books</p>
+
+        )
+    }
+
+}
+
+export default ListBooks
