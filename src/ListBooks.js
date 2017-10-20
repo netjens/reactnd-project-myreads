bf2782988879d7678e6cddae58a4762b698e7684
jens <mailtojens@gmail.com>
2017-10-20 21:45:16 +0200

    fix: equal check

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -20,7 +20,7 @@ class ListBooks extends Component {
                             <h2 className="bookshelf-title">Currently Reading</h2>
                             <div className="bookshelf-books">
                                 <ol className="books-grid">
-                                    {books.filter((book) => (book.shelf == 'currentlyReading')).map((book) => (
+                                    {books.filter((book) => (book.shelf === 'currentlyReading')).map((book) => (
                                         <li key={book.id}>
                                             <Book
                                                 backgroundImage={book.imageLinks.thumbnail}
@@ -35,7 +35,7 @@ class ListBooks extends Component {
                             <h2 className="bookshelf-title">Want to Read</h2>
                             <div className="bookshelf-books">
                                 <ol className="books-grid">
-                                    {books.filter((book) => (book.shelf == 'wantToRead')).map((book) => (
+                                    {books.filter((book) => (book.shelf === 'wantToRead')).map((book) => (
                                         <li key={book.id}>
                                             <Book
                                                 backgroundImage={book.imageLinks.thumbnail}
@@ -51,7 +51,7 @@ class ListBooks extends Component {
                             <h2 className="bookshelf-title">Read</h2>
                             <div className="bookshelf-books">
                                 <ol className="books-grid">
-                                    {books.filter((book) => (book.shelf == 'read')).map((book) => (
+                                    {books.filter((book) => (book.shelf === 'read')).map((book) => (
                                         <li key={book.id}>
                                             <Book
                                                 backgroundImage={book.imageLinks.thumbnail}
