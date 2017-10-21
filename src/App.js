754652e1bebf811950fcf04869f616f1e56d6e02
jens <mailtojens@gmail.com>
2017-10-21 17:11:30 +0200

    fix: changeShelf method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Route} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
@@ -12,31 +12,44 @@ class BooksApp extends Component {
   }
 
   componentDidMount() {
-     BooksAPI.getAll().then((books) => {
-      this.setState({books})
-    })
+    BooksAPI
+      .getAll()
+      .then((books) => {
+        this.setState({books})
+
+      })
+  }
+
+  changeShelf = (book, newShelf) => {
+    
+    BooksAPI.update(book, newShelf)
+    const booksUpdate = this.state.books
+    console.log(booksUpdate)
+    
+   for (let i = 0; i < booksUpdate.length; i++) {
+      if(booksUpdate[i].id === book.id){
+        booksUpdate[i].shelf = newShelf
+        break
+      }
+    }
+    this.setState({books: booksUpdate})
+
   }
 
 
 
-
-  render() { 
+  render() {
     return (
       <div className="app">
-        <Route exact path="/" render={() => (
+        <Route
+          exact
+          path="/"
+          render={() => (<ListBooks onChangeShelf={this.changeShelf} books={this.state.books}/>)}/>
 
-          <ListBooks
-            books={this.state.books} />
-        )} />
-
-        <Route path="/search" 
-          render={({history})=>(
-            <SearchBooks books={this.state.books} 
-        
-         />
-          )}/>
+        <Route
+          path="/search"
+          render={({history}) => (<SearchBooks books={this.state.books}/>)}/>
       </div>
-
 
     )
   }
