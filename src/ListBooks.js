2bbbb0654fa6cb209b460ab2a456c20e03362693
Jens Gebhardt <dt0a35@canda.com>
2017-10-24 16:14:56 +0200

    refactor: change ListBooks class to statless functional component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,39 +1,38 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Bookshelf from './Bookshelf'
 import SHELF from './ShelfConsants'
 
-class ListBooks extends Component {
-    static propTypes = {
-        books: PropTypes.array.isRequired,
-        onChangeShelf: PropTypes.func.isRequired
-    }
 
-    render() {
-        const {books, onChangeShelf} = this.props
+const ListBooks = (props) => {
 
-        return (
+    const { books, onChangeShelf } = props
 
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <Bookshelf books={books} onChangeShelf={onChangeShelf} shelf={SHELF.CURRENTLY_READING} />
-                        <Bookshelf books={books} onChangeShelf={onChangeShelf} shelf={SHELF.WANT_TO_READ} />
-                        <Bookshelf books={books} onChangeShelf={onChangeShelf} shelf={SHELF.READ} />
-                    </div>
-                </div>
-                <div className="open-search">
-                    <Link to='/search'>Add a book</Link>
+    return (
+
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    <Bookshelf books={books} onChangeShelf={onChangeShelf} shelf={SHELF.CURRENTLY_READING} />
+                    <Bookshelf books={books} onChangeShelf={onChangeShelf} shelf={SHELF.WANT_TO_READ} />
+                    <Bookshelf books={books} onChangeShelf={onChangeShelf} shelf={SHELF.READ} />
                 </div>
             </div>
-        )
+            <div className="open-search">
+                <Link to='/search'>Add a book</Link>
+            </div>
+        </div>
+    )
 
-    }
+}
 
+ListBooks.propTypes = {
+    books: PropTypes.array.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
 }
 
 export default ListBooks
